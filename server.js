a8346234f25d969418459197fe1f3714bcf52945
razennnnnnnn <mlanguenakers01@gmail.com>
2025-06-15 22:38:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK;
 
 app.get("/", (req, res) => {
+  const now = new Date();
+  const date = now.toLocaleDateString('fr-FR');
+  const time = now.toLocaleTimeString('fr-FR');
+
   fetch(DISCORD_WEBHOOK_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      content: "📦 Quelqu’un a scanné le QR code ! Le colis est arrivé ✅"
+       content: `📦 **Colis scanné !**
+📍 **Date :** ${date}
+🕒 **Heure :** ${time}
+🔗 _QR code scanné devant chez toi_`
     })
-  });
-  res.send("<h1>Colis confirmé !</h1>");
+  })
+  .then(() => console.log('Webhook Discord envoyé'))
+  .catch(err => console.error('Erreur webhook Discord:', err));
+
+  res.send("<h1>Notification envoyée à Mathieu Languenakers, Merci pour la livraison</h1>");
 });
 
 app.listen(PORT, () => {
